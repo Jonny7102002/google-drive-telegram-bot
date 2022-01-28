855c77571b5ef86203489ff1cd1090613b65a7ac
Jonny7102002 <90916550+Jonny7102002@users.noreply.github.com>
2022-01-28 22:01:18 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -2,7 +2,7 @@ class config:
     BOT_TOKEN = "5054629651:AAHilpCq9bdZsz7L2lkW_kh993QPbt6JdEs"
     APP_ID = "17048077"
     API_HASH = "07df12ea192493e4075db04d4f034ef0"
-    DATABASE_URL = "postgres://zfqnkozcfdcrlf:bd18ea2f62beafec9148f54c8148d0e846afe122293e0acf555c3bbeabef4a7e@ec2-3-216-113-109.compute-1.amazonaws.com:5432/da006n2bnk67ds"
+    DATABASE_URL = "postgres://aeizruftqjqgpb:87060ea64124c8a0f3d3411fa77a698236fb2d4651e15eb1ab02a567e3da9487@ec2-3-212-143-188.compute-1.amazonaws.com:5432/d1cu6ol2meraep"
     SUDO_USERS = " 1137634344" # Sepearted by space.
     SUPPORT_CHAT_LINK = "https://t.me/+BtTNdG49MJY3YmFl"
     DOWNLOAD_DIRECTORY = "./downloads/"
